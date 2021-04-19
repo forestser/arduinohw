39396f79a498aeb9edee82656476372141fad253
forestser <forester00@gmail.com>
2021-04-19 13:54:55 +0900

    3-7 수정

--- LED_number/LED_number.ino
+++ LED_number/LED_number.ino
@@ -7,12 +7,12 @@ pinMode(5, OUTPUT);
 }
 void loop() {
 int i;
-for(i = 1; i<=16; i++)
+for(i = 0; i<=15; i++)
 {
   digitalWrite(2, i%2);
   digitalWrite(3, (i>>1)%2);
   digitalWrite(4, (i>>2)%2);
   digitalWrite(5, (i>>3)%2);
-  delay(500);
+  delay(800);
 }
 }
